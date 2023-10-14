47fad5ada1fc7b8d2b7a7eaebdcfdb3bd531f383
2382546457 <2382546457@qq.com>
2023-10-14 19:14:45 +0800

    AdminBizImpl : 调度中心对于执行器发送过来的消息，做出应对的类

---
+++ scheduled-task/scheduled-task-admin/src/main/java/com/xiaohe/admin/service/impl/AdminBizImpl.java
@@ -0,0 +1,32 @@
+package com.xiaohe.admin.service.impl;
+
+import com.xiaohe.admin.core.thread.JobCompleteHelper;
+import com.xiaohe.admin.core.thread.JobRegistryHelper;
+import com.xiaohe.core.biz.AdminBiz;
+import com.xiaohe.core.model.HandlerCallbackParam;
+import com.xiaohe.core.model.RegistryParam;
+import com.xiaohe.core.model.Result;
+
+import java.util.List;
+
+/**
+ * @author : 小何
+ * @Description : 由JobApiController调用，处理从
+ * @date : 2023-10-14 19:11
+ */
+public class AdminBizImpl implements AdminBiz {
+    @Override
+    public Result callback(List<HandlerCallbackParam> callbackParamList) {
+        return JobCompleteHelper.getInstance().callback(callbackParamList);
+    }
+
+    @Override
+    public Result registry(RegistryParam registryParam) {
+        return JobRegistryHelper.getInstance().registry(registryParam);
+    }
+
+    @Override
+    public Result registryRemove(RegistryParam registryParam) {
+        return JobRegistryHelper.getInstance().registryRemove(registryParam);
+    }
+}
