bad1f20db8eab81e66929acad932d39a72834de5
2382546457 <2382546457@qq.com>
2023-10-21 20:49:46 +0800

    依赖于spring的执行器

---
+++ scheduled-task/scheduled-task-sample/scheduled-task-sample-springboot/src/main/java/com/xiaohe/executor/XxlJobExecutorApplication.java
@@ -0,0 +1,14 @@
+package com.xiaohe.executor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class XxlJobExecutorApplication {
+
+	public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorApplication.class, args);
+	}
+
+}
